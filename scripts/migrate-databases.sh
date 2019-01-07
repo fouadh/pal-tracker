e2fc6ccfe90f89773dbb0078348321961da28c5e
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-07 14:29:19 +0100

    Change tunnel port for db migrations

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -11,12 +11,12 @@ db_username=`echo $credentials | jq -r '.username'`
 db_password=`echo $credentials | jq -r '.password'`
 
 echo "Opening ssh tunnel to $ip_address"
-cf ssh -N -L 63306:$ip_address:3306 pal-tracker &
+cf ssh -N -L 63307:$ip_address:3306 pal-tracker &
 cf_ssh_pid=$!
 
 echo "Waiting for tunnel"
 sleep 5
 
-flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63306/$db_name" -locations=filesystem:$2/databases/tracker -user=$db_username -password=$db_password migrate
+flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63307/$db_name" -locations=filesystem:$2/databases/tracker -user=$db_username -password=$db_password migrate
 
 kill -STOP $cf_ssh_pid
